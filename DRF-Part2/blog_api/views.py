fda12167b2e3bd395c29a542f54f97343cfd96c6
Nguyễn Nhật <ng.nhat103@gmail.com>
2023-03-16 15:31:50 +0700

    part 2

---
+++ DRF-Part2/blog_api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import BasePermission, IsAdminUser, DjangoModelPermissions
+
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = [DjangoModelPermissions]
+    queryset = Post.postobject.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
