e719970ee79ce6e05a16d63cd99dc4c2dd9ce864
Nguyễn Nhật <ng.nhat103@gmail.com>
2023-03-17 16:51:36 +0700

    add part 3 authenticate

---
+++ DRF-Part1/blog_api/views.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.postobject.all()
+    serializer_class = PostSerializer
+    pass
+
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
